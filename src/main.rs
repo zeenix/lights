44436e727b21d195710ca971fed6fd7c48d27d8e
Zeeshan Ali <zeeshanak@gnome.org>
2020-04-17 22:40:29 +0200

    Step up or down based on cmdline arg

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,25 @@ fn main() {
         })
     .unwrap();
 
+    let method = match std::env::args().nth(1) {
+        Some(s) => {
+            if s == "+" {
+                "StepUp"
+            } else if s == "-" {
+                "StepDown"
+            } else {
+                panic!("Expected either '+' or '-' argument. Got: {}", s);
+            }
+        },
+        None => "StepUp",
+    };
+
     let reply = connection
         .call_method(
             Some("org.gnome.SettingsDaemon.Power"),
             "/org/gnome/SettingsDaemon/Power",
             Some("org.gnome.SettingsDaemon.Power.Screen"),
-            "StepDown",
+            method,
             &(),
         )
         .unwrap();
